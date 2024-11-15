191860efacde002d7ca771ad5b3d35dd273837fc
UraAkonka <68127105+UraAkonka@users.noreply.github.com>
2024-11-15 15:46:20 +0500

    Revert "Feature/fun2"

--- main.cpp
+++ main.cpp
@@ -21,20 +21,10 @@ void read(const string& filename, vector<string>& lines) {
 
 // Функция №2: 222
 void print(const vector<string>& lines) {
-    for (const auto& str : lines) {
-        cout << str << endl;
-    }
 }
 // Функция №3: 3333333
 void write(const vector<string>& lines, const string& filename) {
-    ofstream file(filename);
-    if (!file) {
-        cerr << "Ошибка открытия файла для записи!" << endl;
-        return;
-    }
-    for (const auto& str : lines) {
-        file << str << endl;
-    }
+
 }
 int main() {
     setlocale(LC_ALL,"Russian");
