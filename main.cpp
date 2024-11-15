343307fea5d81f63522f4e87e642d3f49bea1f32
UraAkonka <lebzyak321@mail.ru>
2024-11-15 14:29:45 +0500

    Описание изменений

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+// Функция №1
+vector<string> readLinesFromFile(const string& filename) {
+    vector<string> lines;
+    return lines;
+}
+
+// Функция №2
+void printLines(const vector<string>& lines) {
+}
+
+// Функция №3
+void writeLinesToFile(const vector<string>& lines, const string& filename) {
+}
+
+int main() {
+    setlocale(LC_ALL,"Russian");
+    string inputFilename = "input.txt";
+    string outputFilename = "output.txt";
+
+    vector<string> lines = readLinesFromFile(inputFilename);
+    printLines(lines);
+    writeLinesToFile(lines, outputFilename);
+    cout << "Программа завершена." << endl;
     return 0;
 }
